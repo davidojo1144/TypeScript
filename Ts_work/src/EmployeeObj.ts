c77da1291491335e40ea0fa73f86ab999cf4d2b4
davidojo1144 <davco1144@gmail.com>
2025-04-09 10:08:50 +0100

    Working with Typescript

--- Ts_work/src/EmployeeObj.ts
+++ Ts_work/src/EmployeeObj.ts
@@ -4,10 +4,19 @@ type Employee = {
     retire: (date: Date) => void
 }
 
-let employee: Employee = {
+let employee1: Employee = {
     id: 1,
     name: "Dapo",
     retire: (date: Date)=> {
         console.log(date)
+    }
+}
+
+let employee2: Employee = {
+    id: 2,
+    name: "Segun",
+    retire: (date: Date) {
+        console.log(date);
+        
     }
 }
